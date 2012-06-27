603563a590444fb2ff4ebe6f9e32389a8de3c7d2
Chris Eidhof <chris@eidhof.nl>
2012-06-27 11:45:12 +0200

    Initial Commit

---
+++ LinkedInAPI/CEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CEAppDelegate.h
+//  LinkedInAPI
+//
+//  Created by Chris Eidhof on 6/27/12.
+//  Copyright (c) 2012 Chris Eidhof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
